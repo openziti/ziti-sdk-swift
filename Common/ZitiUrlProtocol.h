4e53ef518a7ec7191113b779fb75434a61d531a8
David Hart <dphart@rochester.rr.com>
2020-04-15 21:23:57 -0400

    imported headers

--- Common/ZitiUrlProtocol.h
+++ Common/ZitiUrlProtocol.h
@@ -7,12 +7,24 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for ZitiUrlProtocol_macOS.
+//! Project version number for ZitiUrlProtocol
 FOUNDATION_EXPORT double ZitiUrlProtocol_macOSVersionNumber;
 
-//! Project version string for ZitiUrlProtocol_macOS.
+//! Project version string for ZitiUrlProtocol
 FOUNDATION_EXPORT const unsigned char ZitiUrlProtocol_macOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZitiUrlProtocol_macOS/PublicHeader.h>
 
-
+#import "uv_mbed/um_http.h"
+#import "uv_mbed/queue.h"
+#import "uv_mbed/tls_engine.h"
+#import "uv_mbed/um_http.h"
+#import "uv_mbed/uv_mbed.h"
+#import "uv/darwin.h"
+#import "uv/errno.h"
+#import "uv/threadpool.h"
+#import "uv/unix.h"
+#import "uv/version.h"
+#import "uv_link_t.h"
+#import "uv.h"
+#import "http_parser.h"
