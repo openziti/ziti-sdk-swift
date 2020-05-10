74aa85c0b9224dd3fa93e4b5ae679eeabad840f6
Dave Hart <dphart@rochester.rr.com>
2020-05-10 01:53:45 -0400

    first pass at keychain support

--- ziti-mac-enroller/main.swift
+++ ziti-mac-enroller/main.swift
@@ -24,14 +24,14 @@ guard CommandLine.argc == 2 else {
     exit(-1)
 }
 
-// Enroll
-let loop = uv_default_loop()
-let enroller = ZitiEnroller()
+// some logging...
+ziti_debug_level = 3
 
-enroller.enroll(loop: loop, jwtFile: args[1]) { resp, subj, err in
+// Enroll
+ZitiEnroller().enroll(jwtFile: args[1]) { resp, subj, err in
     guard let resp = resp, let subj = subj else {
         print("Invalid enrollment response, \(String(describing: err))")
-        exit(-3)
+        exit(-1)
     }
     
     #if false
@@ -44,14 +44,124 @@ enroller.enroll(loop: loop, jwtFile: args[1]) { resp, subj, err in
         "\n")
     #endif
     
-    // update keychain for this identity
+    // Strip leading "pem:"s
+    let key = dropFirst("pem:", resp.id.key)
+    let cert = dropFirst("pem:", resp.id.cert)
+    var ca = resp.id.ca
+    if let idCa = resp.id.ca {
+        ca = dropFirst("pem:", idCa)
+    }
     
-    // tell the user the id...
+    // Store private key
+    guard key.starts(with: "-----BEGIN EC PRIVATE KEY-----") else {
+        print("Only keys of type EC are currently supported")
+        exit(-1)
+    }
+    
+    let zkc = ZitiKeychain(tag: subj)
+    guard zkc.storePrivateKey(fromPem: key, kSecAttrKeyTypeECSECPrimeRandom) == nil else {
+        print("Unable to store private key")
+        exit(-1)
+    }
+    
+    // Store certificate
+    guard zkc.storeCertificate(fromPem: cert) == nil else {
+        print("Unable to store certificate")
+        exit(-1)
+    }
+    
+#if false
+    // See if we need to add trust for for the CA
+    if let ca = ca {
+        let certs = ZitiKeychain.extractCerts(ca)
+        
+        // evalTrustForCertificates requires same DispatchQueue as caller, so force that to happen.
+        // use condition to force every to block until we're ready to move on, else program will
+        // exit...
+        let dq = DispatchQueue(label: args[0])
+        dq.async {
+            let cond = NSCondition()
+            var evalComplete = false
+            
+            let status = ZitiKeychain.evalTrustForCertificates(certs, dq) { secTrust, isTrusted, err in
+                defer {
+                    cond.lock()
+                    evalComplete = true
+                    cond.unlock()
+                }
+                
+                print("CA trusted: \(isTrusted)")
+                if let err = err {
+                    print("\(err as Error)")
+                }
+                
+                // if not trusted, prompt to addTrustForCertificate
+                if !isTrusted {
+                    // TODO: addTrustForCertificate is setup to only work for a Root CA, which we may
+                    // not have. We could change that...
+                    guard let rootCa = ZitiKeychain.extractRootCa(ca) else {
+                        print("Unable to extract CA to add trust")
+                        return
+                    }
+                    
+                    let status = ZitiKeychain.addTrustForCertificate(rootCa)
+                    guard status == errSecSuccess else {
+                        let errStr = SecCopyErrorMessageString(status, nil) as String? ?? "\(status)"
+                        print("Unable to add trust for ca, err: \(status), \(errStr)")
+                        return
+                    }
+                }
+            }
+            guard status == errSecSuccess else {
+                let errStr = SecCopyErrorMessageString(status, nil) as String? ?? "\(status)"
+                print("Unable to evaluate trust for ca, err: \(status), \(errStr)")
+                exit(status)
+            }
+            
+            cond.lock()
+            while !evalComplete {
+                if !cond.wait(until: Date(timeIntervalSinceNow: 10.0))  {
+                    print("timed-out waiting for trust evaluation")
+                    cond.unlock()
+                    break
+                }
+            }
+            cond.unlock()
+        }
+    }
+#else
+    // TODO: addTrustForCertificate is setup to only work for a Root CA, which we may
+    // not have. We could change that...
+    if let ca = ca {
+        guard let rootCa = zkc.extractRootCa(ca) else {
+            print("Unable to extract CA to add trust")
+            return
+        }
+        
+        let status = zkc.addTrustForCertificate(rootCa)
+        guard status == errSecSuccess else {
+            let errStr = SecCopyErrorMessageString(status, nil) as String? ?? "\(status)"
+            print("Unable to add trust for ca, err: \(status), \(errStr)")
+            return
+        }
+    }
+#endif
+    
+    // store resp.ztAPI somewhere in keychain..
+    if zkc.storeController(resp.ztAPI) != nil {
+        print("Unable to store controller in keychain")
+        exit(-1)
+    }
+    
+    // tell the user the application tag used to store the data in the keychain
     print("Successfully enrolled id \"\(subj)\" with controller \"\(resp.ztAPI)\"")
 }
 
-// start the loop, exit when processing complete
-ziti_debug_level = 3
-exit(uv_run(loop, UV_RUN_DEFAULT))
-
-
+// Helper
+func dropFirst(_ drop:String, _ str:String) -> String {
+    var newStr = str
+    if newStr.starts(with: drop) {
+        newStr = String(newStr.dropFirst(drop.count))
+    }
+    return newStr
+}
